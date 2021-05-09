63437b2cf1d5fc4ce47e58b073bb05a6913fba8e
phbord <phbord@gmail.com>
2021-05-09 13:32:42 +0200

    seed

--- Gemfile
+++ Gemfile
@@ -63,4 +63,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "table_print"
 gem "faker"
-gem "string_pattern"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-require 'string_pattern'
 
 Faker::Config.locale = "fr"
 
